d61e9612737f906e6a39aa72444c77238c7bcadd
yairmishnayot <yairm@hackeruso.com>
2023-12-27 09:31:23 +0200

    Finished basic user CRUD functionality

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -8,12 +8,17 @@ const User = userModel(sequelize, DataTypes);
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("get all users");
+router.get("/", async (req, res) => {
+    const users = await User.findAll();
+    res.status(200).json(users);
 })
 
-router.get("/:id", (req, res) => {
-    res.send("get a single user");
+router.get("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
 })
 
 router.post("/", async (req, res) => {
@@ -23,12 +28,30 @@ router.post("/", async (req, res) => {
     res.status(201).json(newUser);
 })
 
-router.put("/:id", (req, res) => {
-    res.send("update user");
+router.put("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+    }
+    const { name, email } = req.body;
+    user.name = name;
+    user.email = email;
+    user.save
+    res.status(200).json(user);
 });
 
-router.delete("/:id", (req, res) => {
-    res.send("delete a user");
+router.delete("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+    }
+    await User.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.status(200).json({ message: "User deleted" });
 })
 
 module.exports = router;
