48884b6a2186fa01a948ef9dbf5567f0a9c890c2
Yair <yair543210@gmail.com>
2023-12-30 21:05:56 +0200

    tryCatch util

--- src/api/v1/controllers/UsersController.js
+++ src/api/v1/controllers/UsersController.js
@@ -1,56 +1,45 @@
 const userModel = require('../../../../models/user.js');
 const sequelize = require("../../../config/db.js");
+const { tryCatch } = require('../../../utils/tryCatch.js');
 const DataTypes = require('sequelize').DataTypes;
 const User = userModel(sequelize, DataTypes);
 
 const UsersController = {
-    getAllUsers: async (req, res, next) => {
-        try {
-            const users = await User.findAll();
-            res.status(200).json(users);
-        } catch (err) {
-            next(err)
-        }
-    },
-    getUserById: async (req, res, next) => {
-        try {
-            const user = await User.findByPk(req.params.id);
-            if (!user) {
-                res.status(404);
-                throw new Error("User not found");
-            }
-            res.status(200).json(user);
-        } catch (err) {
-            next(err)
-        }
-    },
-    createUser: async (req, res, next) => {
-        try {
-            const { name, email } = req.body;
-            const newUser = await User.create({ name, email });
+    getAllUsers: tryCatch(async (req, res, next) => {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    }),
 
-            res.status(201).json(newUser);
-        } catch (err) {
-            next(err)
+    getUserById: tryCatch(async (req, res, next) => {
+        const user = await User.findByPk(req.params.id);
+        if (!user) {
+            res.status(404);
+            throw new Error("User not found");
         }
-    },
-    updateUser: async (req, res, next) => {
-        try {
-            const user = await User.findByPk(req.params.id);
-            if (!user) {
-                res.status(404);
-                throw new Error("user not found");
-            }
-            const { name, email } = req.body;
-            user.name = name;
-            user.email = email;
-            user.save();
-            res.status(200).json(user);
-        } catch (error) {
-            next(err)
+        res.status(200).json(user);
+    }),
+
+    createUser: tryCatch(async (req, res, next) => {
+        const { name, email } = req.body;
+        const newUser = await User.create({ name, email });
+
+        res.status(201).json(newUser);
+    }),
+
+    updateUser: tryCatch(async (req, res, next) => {
+        const user = await User.findByPk(req.params.id);
+        if (!user) {
+            res.status(404);
+            throw new Error("user not found");
         }
-    },
-    deleteUser: async (req, res, next) => {
+        const { name, email } = req.body;
+        user.name = name;
+        user.email = email;
+        user.save();
+        res.status(200).json(user);
+    }),
+
+    deleteUser: tryCatch(async (req, res, next) => {
         const user = await User.findByPk(req.params.id);
         if (!user) {
             res.status(404).json({ error: "User not found" });
@@ -62,7 +51,7 @@ const UsersController = {
         });
 
         res.status(200).json({ message: "User deleted" });
-    }
+    })
 }
 
 module.exports = UsersController;
